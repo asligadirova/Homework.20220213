167d652f9def2946a272a001a57b9b1d2504e88f
asligadirova <aslisg@code.edu.az>
2022-02-15 23:27:53 +0400

    Add project files.

---
+++ Task6.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Latin1;
+
+            string word = "1) Verilmish metnde {a} simvolun sayi {b} simvolun sayinda nece defe coxdur?"
+;
+            int countA = 0;
+            int countB = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    countA++;
+                }
+                else if (word[i] == 'b')
+                {
+                    countB++;
+                }
+            }
+            Console.WriteLine(countB);
+            Console.WriteLine(countA);
+
+            Console.WriteLine("******");
+
+            int c = countA / countB;
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
---
+++ Task6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "2) Verilmish metnde sol terefden tek yerde dayanan simvollarin hamisi {a} simvoludurmu?";
+            
+
+        }
+    }
+}
